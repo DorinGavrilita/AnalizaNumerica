89ebc2ad847170aa7a644dfe0613326071832eb4
Gavrilita Dorin <gavrilitadorin5@gmail.com>
2023-02-16 11:22:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <stdio.h>
+#include <cmath>
 
 using namespace std;
+
 struct Interval
 {
     double left;
@@ -14,6 +17,8 @@ double f(double x)
 int Separation(double A, double B, double h);
 double Bisection(double a, double b, int &k);
 
+eps =
+
 int main()
 {
     int i, n;
@@ -23,10 +28,12 @@ int main()
     n = Separation(A, B, h);
     for(i=0; i<n; i++)
     {
+        cout << ""
         cout << "O solutie se afla pe [" << rada[i].left << "; " << rada[i].right << "]" << endl;
     };
     return 0;
 }
+
 int Separation(double A, double B, double h)
 {
     int n = 0;
@@ -42,4 +49,65 @@ int Separation(double A, double B, double h)
         x0 = x;
     }  while(x<B);
     return n;
+}
+int Bisection(double a, double b, int &k)
+{
+    double x;
+    k = 0;
+    do
+    {
+        k++;
+        x = (a+b)/2;
+        if(fabs(f(x))<eps) return x;
+        if(f(a)*f(x) < 0) b = x;
+        else a =x;
+    }  while(fabs(b-a) > eps);
+    return (a+b)/2;
+}
+ double Newton(double a, double b, int &k)
+ {
+    double x0, x, delta;
+    k = 0;
+    x0 = (a+b)/2;
+    do
+    {
+        k++;
+        x = x0 - f(x0)/fpr(x0);
+        if(fabs(f(x))<eps) return x;
+        delta = (fabs(x0-x);
+        x0 =x;
+    }  while (delta > eps);
+    return x;
+}
+double ModificataTabgentelor(double a, double b, int &k)
+ {
+    double x0, x, delta;
+    k = 0;
+    x0 = (a+b)/2;
+    do
+    {
+        k++;
+        x = x0 - f(x0)/fpr0;
+        if(fabs(f(x))<eps) return x;
+        delta = (fabs(x0-x);
+        x0 = x;
+    }  while (delta > eps);
+    return x;
+}
+
+double Coardelor(double a, double b, int &k)
+ {
+    double x0, x1, x, delta;
+    k = 1;
+    x0 = a; x1 = b;
+    do
+    {
+        k++;
+        x = x1 - f(x01)*(X1 - X0)/(f(x1) - f(x0));
+        if(fabs(f(x))<eps) return x;
+        delta = (fabs(x-x1);
+        x0 = x1;
+        x1 = x
+    }  while (delta > eps);
+    return x;
 }
